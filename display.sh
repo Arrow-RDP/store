6778510d1e3af2b2dd2d5db9e9d29aeacb5a9a64
manithnoon <56628844+manithnoon@users.noreply.github.com>
2023-11-23 20:06:37 +0700

    Update and rename display to display.sh

--- display.sh
+++ display.sh
@@ -1,1 +1,2 @@
+#!/bin/bash
 DISPLAY= /opt/google/chrome-remote-desktop/start-host --code="4/0AfJohXkemIzQh-ggP6KP24UGmmdtTJLviy0c3sWmQah7SugABMnbu95D4b4mj_OO6ESUlQ" --redirect-url="https://remotedesktop.google.com/_/oauthredirect" --name=$(hostname)
